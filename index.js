7f1789d5ab7f5655774a91f4147d1ce7da0a790e
EmilyDewdrop <turner.ek@gmail.com>
2023-03-02 22:52:32 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+function getfilm(plot, cast, runtime, rating, year) {console.log(plot, cast, runtime, rating, year)}
+
+
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+const movieData = {
+    "The Darjeeling Limited": {
+      plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+      cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+      runtime: 151,
+      rating: 7.2,
+      year: 2007,
+    },
+    "The Royal Tenenbaums": {
+      plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+      rating: 7.6,
+      year: 2001,
+      cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+      runtime: 170,
+    },
+    "Fantastic Mr. Fox": {
+      year: 2009,
+      plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+      cast: [
+        "George Clooney",
+        "Meryl Streep",
+        "Bill Murray",
+        "Jason Schwartzman",
+      ],
+      runtime: 147,
+      rating: 7.9,
+    },
+    "The Grand Budapest Hotel": {
+      rating: 8.1,
+      runtime: 159,
+      year: 2014,
+      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+      cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+    },
+  };
+
+  document.getElementById("dl").onclick = movieData["The Darjeeling Limited"]
+
+  function displayMovieData() {
+    document.getElementById("movietext").innerHTML = Date(getfilm);
+  }
